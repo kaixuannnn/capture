bdfcb83fdc7a3c29fd999cae7fb933a28c92cc3b
kaixuannnn <kaixuant@yahoo.com>
2020-11-30 15:12:49 +0800

    4. styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import AboutUs from './pages/AboutUs';
+import GlobalStyle from './Components/GlobalStyle'
 
 function App() {
   return (
     <div className="App">
-     <h1>Film</h1>
+     <GlobalStyle />
      <AboutUs />
     </div>
   );
