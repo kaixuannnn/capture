620a872139fa29e6f14876f50cc6eb38bdb7f6d3
kaixuannnn <kaixuant@yahoo.com>
2020-12-04 16:04:12 +0800

    our work animation

--- src/Components/AboutSection.js
+++ src/Components/AboutSection.js
@@ -2,6 +2,8 @@ import React from 'react';
 import home1 from '../img/home1.png';
 import { About, Description, Image, Hide} from "../styles";
 import {motion} from "framer-motion";
+import {titleAnim, fade, photoAnim} from '../animation';
+import Wave from './Waves';
 
 const AboutSection= () =>{
 
@@ -10,22 +12,23 @@ const AboutSection= () =>{
             <Description>
                 <motion.div>
                     <Hide>
-                        <motion.h2>We work to make</motion.h2>
+                        <motion.h2 variants={titleAnim}>We work to make</motion.h2>
                     </Hide>
                     <Hide className="hide">
-                        <motion.h2>your <span>dreams</span> come</motion.h2>
+                        <motion.h2 variants={titleAnim}>your <span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide className="hide">
-                        <motion.h2>true.</motion.h2>
+                        <motion.h2 variants={titleAnim}>true.</motion.h2>
                     </Hide>
                 </motion.div>
-                <p> Contact us for any phorography or videography ideas that ypu have. We have professionals with amazing skills.
-                </p>
-                <button>Contact Us</button>
+                <motion.p variants={fade}> Contact us for any phorography or videography ideas that ypu have. We have professionals with amazing skills.
+                </motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="guy with a camera"/>
+                <motion.img variants={photoAnim} src={home1} alt="guy with a camera"/>
             </Image>
+            <Wave />
         </About>
     )
 }
