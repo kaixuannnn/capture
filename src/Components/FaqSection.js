c39594086cbd0c936923a23401a4789b87d783b8
kaixuannnn <kaixuant@yahoo.com>
2020-12-04 16:32:01 +0800

    toggle component with children

--- src/Components/FaqSection.js
+++ src/Components/FaqSection.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 import {About} from '../styles';
+import Toggle from './Toggle';
 
 const FaqSection =() =>{
     return(
         <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
+            <Toggle>
             <div className="question">
                 <h4>How Do I start?</h4>
-                <div className="answer">
+                    <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, inventore?</p>
-                </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, inventore?</p> 
+                    </div>
                 <div className="faq-line"></div>
-            </div>
+                </div>
+                </Toggle>
             <div className="question">
                 <h4>Daily Schedule</h4>
                 <div className="answer">
