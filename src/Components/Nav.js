afeee083160682fe9f61c12d00dd1ffc8ac2fd0c
kaixuannnn <kaixuant@yahoo.com>
2020-12-05 14:03:08 +0800

    modify

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -11,15 +11,15 @@ const Nav = () => {
             <h1><Link id="logo" to="/">Capture</Link></h1>
             <ul>
                 <li>
-                    <Link to="/">1. About Us</Link>
+                    <Link to="/the-react-course-capture">1. About Us</Link>
                     <Line transition={{duration: 0.75}} initial={{width:"0%"}} animate={{width: pathname === "/" ? "50%":"0%"}}></Line>
                 </li>
                 <li>
-                    <Link to="/work">1. Our Work</Link>
+                    <Link to="/the-react-course-capture/work">1. Our Work</Link>
                     <Line transition={{duration: 0.75}} initial={{width:"0%"}} animate={{width: pathname === "/work" ? "50%":"0%"}}></Line>
                 </li>
                 <li>
-                    <Link to="/contact">1. Contact Us</Link>
+                    <Link to="/the-react-course-capture/contact">1. Contact Us</Link>
                     <Line transition={{duration: 0.75}} initial={{width:"0%"}} animate={{width: pathname === "/contact" ? "50%":"0%"}}></Line>
                 </li>
             </ul>
